149598a2514ec1bf51263674756ee3b86db854e5
Ihar Yakimush <Ihar_Yakimush@epam.com>
2022-04-22 17:03:07 +0300

    azure emulator

---
+++ StaticWebApp/run_azure_emulator.ps1
@@ -0,0 +1,5 @@
+#npm install -g @azure/static-web-apps-cli azure-functions-core-tools
+#npm --prefix run build
+#swa start ./staticwebapp.react/build --api-location http://localhost:7071
+
+swa start http://localhost:3000 --api-location http://localhost:7071
