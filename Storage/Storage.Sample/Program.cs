cfb27fb2f4c7b9a502f8e8d612b06d408afe3598
Ihar Yakimush <Ihar_Yakimush@epam.com>
2022-05-05 16:26:41 +0300

    queue

--- Storage/Storage.Sample/Program.cs
+++ Storage/Storage.Sample/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Storage.Sample;
+
 Console.WriteLine("Hello, World!");
+
+QueueSample.Run();
---
+++ Storage/Storage.Sample/QueueSample.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Storage.Queues;
+
+namespace Storage.Sample;
+
+public class QueueSample
+{
+    public static void Run()
+    {
+        QueueClient client = new QueueClient("UseDevelopmentStorage=true", "qwe");
+
+        client.CreateIfNotExists();
+    }
+}
